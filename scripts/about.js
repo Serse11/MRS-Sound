47f0e5d832a43849101db970793354212292080f
varionlucie <varion.lucie@hotmail.fr>
2022-05-30 14:45:28 +0200

    responsive and navbar modification

---
+++ scripts/about.js
@@ -0,0 +1,11 @@
+//navbar
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-210px";
+  }
+  prevScrollpos = currentScrollPos;
+}
